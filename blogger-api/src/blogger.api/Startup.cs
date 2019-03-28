04ec22c4fd04d2e8b466e886f04a67cc2d70055f
Lucas Marques <lucas.msilva09@gmail.com>
2019-03-27 23:24:48 -0300

    Creating the layers domain, infra and tests

--- blogger-api/src/blogger.api/Startup.cs
+++ blogger-api/src/blogger.api/Startup.cs
@@ -26,6 +26,7 @@ namespace blogger.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,12 @@ namespace blogger.api
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x =>
+            {
+                x.AllowAnyHeader();
+                x.AllowAnyMethod();
+                x.AllowCredentials();
+            });
             app.UseMvc();
         }
     }
---
+++ blogger-api/src/blogger.infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace blogger.infra
+{
+    public class Class1
+    {
+    }
+}
