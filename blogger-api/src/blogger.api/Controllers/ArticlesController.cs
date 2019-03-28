4df743b2704f6e2df3f26f3fb2b9ad58d78dea44
Lucas Marques da Silva <lmarques@micromidia-1.local>
2019-03-28 19:03:04 -0300

    Creating the first complete flow from the controller to the database
    This commit contains a simple process to insert data in the database

--- blogger-api/src/blogger.api/Controllers/ArticlesController.cs
+++ blogger-api/src/blogger.api/Controllers/ArticlesController.cs
@@ -19,7 +19,7 @@ namespace blogger.api.Controllers
         }
 
         // GET: api/Articles/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
--- blogger-api/src/blogger.infra/Repository/UserRepository.cs
+++ blogger-api/src/blogger.infra/Repository/UserRepository.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using blogger.domain.Entities;
+using blogger.domain.Repositories;
+using blogger.domain.ValueObjects;
+using blogger.infra.DataContext;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace blogger.infra.Repository
 {
-    class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly BloggerDataContext _context;
+        public UserRepository(BloggerDataContext context)
+        {
+            _context = context;
+        }
+        public bool CheckEmail(Email email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(User user)
+        {
+            _context.Connection.Execute("USER_SPI", new
+            {
+                Id = user.Id,
+                FirstName = user.Name.FirstName,
+                LastName = user.Name.LastName,
+                Email = user.Email.Address,
+                Password = user.Password,
+                Active = user.Active,
+                Administrator = user.Administrator
+            }, commandType: CommandType.StoredProcedure);
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
