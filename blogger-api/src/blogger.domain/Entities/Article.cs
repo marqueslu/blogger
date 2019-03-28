c620e331ae2ac0c684af9d53b0180ac186a2b31f
Lucas Marques <lucas.msilva09@gmail.com>
2019-03-27 23:45:02 -0300

    Creating the Article entity

---
+++ blogger-api/src/blogger.domain/Entities/Article.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blogger.domain.Entities
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
