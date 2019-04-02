d06324f2203899e565461f0c065e8af45d59358b
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-04-02 16:36:12 -0300

    Creating the base to the users list

--- blogger-app/src/app/app-routing.module.ts
+++ blogger-app/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'articles' },
-  { path: 'articles', loadChildren: './articles/articles.module#ArticlesModule' }
+  { path: "", pathMatch: "full", redirectTo: "articles" },
+  {
+    path: "articles",
+    loadChildren: "./articles/articles.module#ArticlesModule"
+  },
+  { path: "users", loadChildren: "./users/users.module#UsersModule" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
