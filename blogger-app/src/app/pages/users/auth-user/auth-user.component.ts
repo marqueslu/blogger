0958962b6e9a674b28a55d1c05a6eddbd8125fa3
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-04-05 15:55:16 -0300

    Creating the login page

--- blogger-app/src/app/pages/users/auth-user/auth-user.component.ts
+++ blogger-app/src/app/pages/users/auth-user/auth-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-user',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthUserComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+ 
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
